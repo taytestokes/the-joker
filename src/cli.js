b2354f2cdd98cddc4fdf47be41f9cedb54c28b60
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-17 15:37:19 -0600

    added a cli image when you start the script

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,19 @@
 const program = require('commander');
 const chalk = require('chalk');
+const { prompt } = require('inquirer');
+const emailPrompt = require('./prompts/email');
+const figlet = require('figlet');
+
+// Initial Connection
+console.clear();
+console.log(
+    chalk.magenta(
+        figlet.textSync('The Joker', {
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        })
+    )
+);
 
 // Commander Config
 program
@@ -7,6 +21,14 @@ program
     .description(chalk.yellow('A CLI Application To Send Emails With Random Jokes'));
 
 // Commands
+program
+    .command('send joke')
+    .alias('new')
+    .description('Send a joke to @email')
+    .action(() => {
+        prompt(emailPrompt.questions).then()
+    });
+
 
 // Parse The Arguments
 program.parse(process.argv);
---
+++ src/prompts/email.js
@@ -0,0 +1,13 @@
+// the questions that will be asked when prompted to send an email
+exports.questions = [
+    {
+        type: 'input',
+        name: 'to',
+        message: 'Please enter the @email you would like to joke'
+    },
+    {
+        type: 'input',
+        name: 'subject',
+        message: 'Please enter the subject of the email'
+    }
+];
