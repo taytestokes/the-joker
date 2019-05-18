7a4f3d09db17a2ad6d815db9e083b947fdfae76f
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-17 19:07:43 -0600

    added an option to recieve a link to the github repo

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,7 @@ const figlet = require('figlet');
 const emailSender = require('./commands/sendemail');
 
 // Initial Connection
-    // these will log to create the banner of the app
+// these will log to create the banner of the app
 console.clear();
 console.log(
     chalk.magenta(
@@ -21,18 +21,24 @@ console.log(chalk.green('Thank you for using my app and have fun sending jokes!'
 
 // Commander Config
 program
-    .version('1.0,0')
-    .description(chalk.yellow('A CLI Application To Send Emails With Random Jokes'));
+    .version('1.0.0')
+    .description(chalk.yellow('A CLI Application To Send Emails With Random Jokes'))
+    .option('-g, --github', 'output a link to the github repository')
 
 // Commands
 program
-    .command('joker new')
-    .alias('jn')
+    .command('new')
+    .alias('n')
     .description('Send a joke to @email')
     .action(() => {
         prompt(emailPrompt.questions).then(answers => emailSender.sendmail(answers));
     });
 
+program
+    .command('--github')
+    .alias('-g')
+    .description('Will log a link to the github repository')
+    .action(chalk.cyan(console.log('https://github.com/Tstokes9328/the-joker-cli')));
 
 // Parse The Arguments
 program.parse(process.argv);
