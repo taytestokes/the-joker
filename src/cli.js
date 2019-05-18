2f8ce429d6f6045bafac2d5a0ebb945743bc91b8
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-17 20:05:07 -0600

    updated version

--- src/cli.js
+++ src/cli.js
@@ -21,7 +21,7 @@ console.log(chalk.green('Thank you for using my app and have fun sending jokes!'
 
 // Commander Config
 program
-    .version('1.0.1')
+    .version('1.0.2')
     .description(chalk.yellow('A CLI Application To Send Emails With Random Jokes'))
     .option('-g, --github', 'output a link to the github repository')
 
